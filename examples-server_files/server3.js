efdf48ac89a3db939ff5c9d3a57ac22d232919e7
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-08-09 22:50:27 +0100

    Adding comments to server. Changed port number

--- examples-server_files/server3.js
+++ examples-server_files/server3.js
@@ -1,9 +1,11 @@
 var express = require('express'),
   fs = require('fs');
- 
+
+//instantiate express
 var app = express();
  
+//attach route to app
 require('./routes')(app);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(5000);
+console.log('Listening on port 5000...');
